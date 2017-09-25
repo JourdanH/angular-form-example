c6facf4be19bf37a3de6bc0a61b662e5a0041713
sparrowlark <30511105+sparrowlark@users.noreply.github.com>
2017-09-25 09:36:00 -0700

    adding two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,12 @@ export class AppComponent {
   title = 'app';
 
 
-onSubmit(data: NgForm){
-  console.log(data.value)
-}
+  model: object = {
+    first_name: "Bob",
+    last_name: "Smith"
+  }
+
+  onSubmit(){
+    console.log(this.model)
+  }
 }
